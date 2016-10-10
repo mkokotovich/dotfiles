21dcf934001b92c46ab6dd6e3e1bb6d0f6ff3efe
Matthew Kokotovich <mkokotovich@gmail.com>
2016-10-10 15:06:55 -0400

    Adding a few more files

---
+++ vim/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+echo "Installing pathogen.vim"
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Installing cscope maps"
+mkdir ~/.vim/bundle/cscope_maps/plugin/
+cp $(dirname $0)/cscope_maps.vim ~/.vim/bundle/cscope_maps/plugin/
+
