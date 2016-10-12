48b985a220b414e72c7611584660c8777cfe6c6d
Matthew Kokotovich <mkokotovich@gmail.com>
2016-10-12 01:55:46 +0000

    Fix several issues

--- vim/install.sh
+++ vim/install.sh
@@ -3,6 +3,6 @@ echo "Installing pathogen.vim"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing cscope maps"
-mkdir ~/.vim/bundle/cscope_maps/plugin/
+mkdir -p ~/.vim/bundle/cscope_maps/plugin/
 cp $(dirname $0)/cscope_maps.vim ~/.vim/bundle/cscope_maps/plugin/
 
