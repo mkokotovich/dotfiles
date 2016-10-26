92a2e115fa522c3e8596d094344b4bd68cec87ef
Matthew Kokotovich <mkokotovich@gmail.com>
2016-10-26 11:56:29 -0500

    Adding arduino syntax highlighting

--- vim/install.sh
+++ vim/install.sh
@@ -5,4 +5,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing cscope maps"
 mkdir -p ~/.vim/bundle/cscope_maps/plugin/
 cp $(dirname $0)/cscope_maps.vim ~/.vim/bundle/cscope_maps/plugin/
-
+echo "Installing arduino syntax"
+mkdir -p ~/.vim/bundle/arduino/syntax
+curl -LSso ~/.vim/bundle/arduino/syntax/arduino.vim http://www.vim.org/scripts/download_script.php?src_id=23284
