1ec713b14773c04e8b403c7fbb638d7a482d4d2c
Matthew Kokotovich <mkokotovich@gmail.com>
2017-01-07 20:35:44 -0600

    New vim plugins for angular

--- vim/install.sh
+++ vim/install.sh
@@ -8,3 +8,7 @@ cp $(dirname $0)/cscope_maps.vim ~/.vim/bundle/cscope_maps/plugin/
 echo "Installing arduino syntax"
 mkdir -p ~/.vim/bundle/arduino/syntax
 curl -LSso ~/.vim/bundle/arduino/syntax/arduino.vim http://www.vim.org/scripts/download_script.php?src_id=23284
+echo "Installing vim-js-indent"
+git clone https://github.com/jason0x43/vim-js-indent.git ~/.vim/bundle/vim-js-indent
+echo "Installing typescript-vim"
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/bundle/typescript-vim
