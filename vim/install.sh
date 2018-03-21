6fc971dc096213e8052209426a373bdd78f67d7b
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-03-21 14:47:04 -0500

    Add better python syntax highlighting

--- vim/install.sh
+++ vim/install.sh
@@ -9,6 +9,11 @@ echo "Installing arduino syntax"
 mkdir -p ~/.vim/bundle/arduino/syntax
 curl -LSso ~/.vim/bundle/arduino/syntax/arduino.vim http://www.vim.org/scripts/download_script.php?src_id=23284
 echo "Installing vim-js-indent"
+rm -rf ~/.vim/bundle/vim-js-indent
 git clone https://github.com/jason0x43/vim-js-indent.git ~/.vim/bundle/vim-js-indent
 echo "Installing typescript-vim"
+rm -rf ~/.vim/bundle/typescript-vim
 git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/bundle/typescript-vim
+echo "Installing python-syntax"
+rm -rf ~/.vim/bundle/python-syntax
+git clone https://github.com/vim-python/python-syntax.git ~/.vim/bundle/python-syntax
